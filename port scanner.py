87ca8ed9b8d5f9bfcf9c372d33223368a8bb7e02
gupcoteles <126504821+gupcoteles@users.noreply.github.com>
2024-03-09 20:16:22 +0300

    initial

---
+++ port scanner.py
@@ -0,0 +1,28 @@
+import socket
+import time
+
+def port_tara(target, firstPort, lastPort):
+    print(f"target IP: {target}")
+    print(f"Port Range: {firstPort} - {lastPort}")
+    print("-" * 40)
+
+    for port in range(firstPort, lastPort + 1):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.5)
+            result = s.connect_ex((target, port))
+            if result == 0:
+                print(f"Port {port} open")
+        except socket.gaierror:
+            print(f"The target's ip cannot be reached.")
+        except Exception as error:
+            print(f"Error: {error}")
+        finally:
+            s.close()
+
+if __name__ == "__main__":
+    target = input("Enter the IP address of the target: ")
+    firstPort = int(input("Enter the start port: "))
+    lastPort = int(input("Enter the end port: "))
+
+    port_tara(target, firstPort, lastPort)
